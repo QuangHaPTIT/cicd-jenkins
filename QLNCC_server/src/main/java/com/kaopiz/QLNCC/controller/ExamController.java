3246134cdc1b8be10e7e874a67274f354e4482a1
HaPQ <phamha220203@gmail.com>
2026-04-20 15:36:31 +0700

    test cicd

--- QLNCC_server/src/main/java/com/kaopiz/QLNCC/controller/ExamController.java
+++ QLNCC_server/src/main/java/com/kaopiz/QLNCC/controller/ExamController.java
@@ -1,12 +1,10 @@
 package com.kaopiz.QLNCC.controller;
 
 import com.kaopiz.QLNCC.model.request.ExamSubmitRequest;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -17,5 +15,10 @@ public class ExamController {
     public ResponseEntity<Void> submit(@RequestBody ExamSubmitRequest body) {
         log.info("Exam FE -> BE (no persist): title={}, note={}", body.title(), body.note());
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/me")
+    public String getMe() {
+        return "I AM DEV";
     }
 }
